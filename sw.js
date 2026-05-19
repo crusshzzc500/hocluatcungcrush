8b93a4e7a136644fb0c31b2489e7e707b6ab65b0
Hoang Minh <hoangngocminh333444@gmail.com>
2026-05-19 21:30:15 +0700

    fix firebase save score

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hoc-luat-firebase-v2';
+const CACHE_NAME = 'hoc-luat-firebase-v3';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
